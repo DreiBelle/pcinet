cf101f76c228d2f3609857bcbe148f26e1e1e8a7
dreibelle <dontbeshyyu1@gmail.com>
2023-06-30 20:03:44 -0700

    controller

--- application/views/ComputerService_View.php
+++ application/views/ComputerService_View.php
@@ -1,11 +1,20 @@
 <html>
 
 <head>
-<title>Computer Service</title>
+    <title>Computer Service</title>
+
+    <style>
+        #contents {
+            margin-left: 200px;
+            padding: 20px;
+        }
+    </style>
 </head>
 
 <body>
-    <?php include 'AdminDashboard_View.php'; ?>
+    <div id="contents">
+        computer service
+    </div>
 </body>
 
 </html>
